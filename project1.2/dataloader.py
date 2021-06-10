32cd2488fcac5a7ac4d4ed81bd66db77a70f7513
Anna Puggaard <anna-puggaard@hotmail.com>
2021-06-10 11:36:51 +0200

    Added stuff
    Co-authored-by: Joachim Emanuel Svendsen <joachimes@users.noreply.github.com>
    Co-authored-by: Anders Thuesen <andersbthuesen@gmail.com>

--- project1.2/dataloader.py
+++ project1.2/dataloader.py
@@ -3,19 +3,22 @@ import torch
 import numpy as np
 import glob
 import PIL.Image as Image
-from torchvision import datasets
-import torchvision.transforms as transforms 
+import gdown
 
-def get_svhn(train, transform, batch_size=64):
-    """Get SVHN dataset loader."""
-    # image pre-processing
-    path = 'SVHN/test/'
-    if train:
-        path = 'SVHN/train/'
-    # dataset and data loader
-    svhn_dataset = datasets.SVHN(root=path,
-                                 split='train' if train else 'test',
-                                 transform=transform,
-                                 download=True)
-    
-    return svhn_dataset
+class SVHNBackgroundImages(torch.utils.data.Dataset):
+    def __init__(self, train, transform, data_path='SVHN/SVHN/background'):
+        self.transform = transform
+        data_path = os.path.join(data_path, 'train' if train else 'test')
+        self.image_paths = glob.glob(data_path + '/*.png')
+        
+    def __len__(self):
+        'Returns the total number of samples'
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        'Generates one sample of data'
+        image_path = self.image_paths[idx]
+        
+        image = Image.open(image_path)
+        X = self.transform(image)
+        return X, 10 # Always return class 10, since all are background images
--- project1.2/processBackground.py
+++ project1.2/processBackground.py
@@ -15,44 +15,45 @@ if __name__ == '__main__':
             except:
                 print('Invalid file')
     
-    directory = 'SVHN/SVHN/train/'
-    background_dir = directory + 'background/'
-    if not os.path.exists(background_dir): 
-        os.mkdir(background_dir)
 
-    i = 0
-    for filename in os.listdir(directory):
-        if filename.endswith('.png'):
-            f = os.path.join(directory, filename)
+    for directory in ["SVHN/SVHN/train/", "SVHN/SVHN/test/"]:
+        background_dir = directory + 'background/'
+        if not os.path.exists(background_dir): 
+            os.mkdir(background_dir)
 
-            if os.path.isfile(f):
-                f_name = filename[:-4] 
-                print(f_name)
-                bbox_path = directory + f_name + '.csv'
-                img = plt.imread(f)
-                if img.shape[0] < digit_shape or img.shape[1] < digit_shape:
-                    print(f'Skipping {f_name}')
-                    continue
-                bboxs = np.genfromtxt(bbox_path, delimiter=',')
-                _, left, _, width, _ = bboxs[1]
-                right = left+width
-                for j in range(1,len(bboxs)):
-                    # left most point
-                    if left > bboxs[j,1]:
-                        left = bboxs[j,1]
+        i = 0
+        for filename in os.listdir(directory):
+            if filename.endswith('.png'):
+                f = os.path.join(directory, filename)
 
-                    if right < bboxs[j,1]+bboxs[j,3]:
-                        right = bboxs[j,1]+bboxs[j,3]
-                left, right = int(left), int(right)
-                print(left, right, img.shape)
-                for k in range(0, img.shape[1]-digit_shape-1, digit_shape):
-                    for l in range(0, img.shape[0]-digit_shape-1, digit_shape):
-                        # if overlap with digit label - continue
-                        if (k > left and k < right) or (k+digit_shape > left and k+digit_shape < right):
-                            continue
+                if os.path.isfile(f):
+                    f_name = filename[:-4] 
+                    print(f_name)
+                    bbox_path = directory + f_name + '.csv'
+                    img = plt.imread(f)
+                    if img.shape[0] < digit_shape or img.shape[1] < digit_shape:
+                        print(f'Skipping {f_name}')
+                        continue
+                    bboxs = np.genfromtxt(bbox_path, delimiter=',')
+                    _, left, _, width, _ = bboxs[1]
+                    right = left+width
+                    for j in range(1,len(bboxs)):
+                        # left most point
+                        if left > bboxs[j,1]:
+                            left = bboxs[j,1]
 
-                        plt.imsave(f'{background_dir}{f_name}_{k}{l}.png', img[l:l+digit_shape,k:k+digit_shape])
-            i += 1
-            print(i)
-            if i == 5: break
+                        if right < bboxs[j,1]+bboxs[j,3]:
+                            right = bboxs[j,1]+bboxs[j,3]
+                    left, right = int(left), int(right)
+                    print(left, right, img.shape)
+                    for k in range(0, img.shape[1]-digit_shape-1, digit_shape):
+                        for l in range(0, img.shape[0]-digit_shape-1, digit_shape):
+                            # if overlap with digit label - continue
+                            if (k > left and k < right) or (k+digit_shape > left and k+digit_shape < right):
+                                continue
+
+                            plt.imsave(f'{background_dir}{f_name}_{k}{l}.png', img[l:l+digit_shape,k:k+digit_shape])
+                i += 1
+                print(i)
+                if i == 5: break
 
